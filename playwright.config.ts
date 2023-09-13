a67b7b5f153f3d749213b738686b192cc68ae74b
Igor <merezhkoigor92@gmail.com>
2023-09-13 22:33:51 +0200

    playwright and axios

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,9 +10,9 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './e2e',
+  testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -40,7 +40,7 @@ export default defineConfig({
     // {
     //   name: 'firefox',
     //   use: { ...devices['Desktop Firefox'] },
-    // },
+    //},
 
     // {
     //   name: 'webkit',
@@ -69,9 +69,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+   //webServer: {
+     //command: 'npm run start',
+     //url: 'http://127.0.0.1:3000',
+     //reuseExistingServer: !process.env.CI,
+  //},
 });
