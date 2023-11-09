bb4ecfac44fc21f3d34256c13821b6f724fa9e42
Igor <merezhkoigor92@gmail.com>
2023-11-09 15:19:38 +0300

    added faker

--- constant.ts
+++ constant.ts
@@ -5,9 +5,3 @@ export let standardUserCredentials = {
   userName: "standard_user",
   userPassword: "secret_sauce",
 };
-
-export let clientInformation = {
-  clientFirstName: 'FirstNameTest',
-  clientLastName: 'LastNameTest',
-  clientPostCode: 'PostCodeTest'
-};
--- tests/ui_tests/login.spec.ts
+++ tests/ui_tests/login.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect, Page } from "@playwright/test";
+import { faker } from "@faker-js/faker";
 import { LoginPage } from "../../pages/loginPage";
 import { DashboardPage } from "../../pages/dashboardPage";
 import { standardUserCredentials } from "../../constant";
@@ -6,11 +7,15 @@ import { standardUserCredentials } from "../../constant";
 let page: Page;
 let loginPage: LoginPage;
 let dashboardPage: DashboardPage;
+let randomName: string;
+let randomPassword: string;
 
 test.beforeAll(async ({ browser: Browser }) => {
   page = await Browser.newPage();
   loginPage = new LoginPage(page);
   dashboardPage = new DashboardPage(page);
+  randomName =  await faker.person.firstName();
+  randomPassword = await faker.string.alphanumeric(10);
 });
 
 test.beforeEach(async () => {
@@ -21,13 +26,13 @@ test("User can login with valid credentials", async () => {
   await loginPage.fillUserNameInput(standardUserCredentials.userName);
   await loginPage.fillPasswordInput(standardUserCredentials.userPassword);
   await loginPage.clickLoginButton();
-  const isDashboardPageLoaded = dashboardPage.isDashboardPageLoaded();
+  const isDashboardPageLoaded = await dashboardPage.isDashboardPageLoaded();
   await expect(isDashboardPageLoaded).toBeTruthy();
 });
 
 test("User can't login with invalid credentials", async () => {
-  await loginPage.fillUserNameInput("TestName");
-  await loginPage.fillPasswordInput("TestPassword");
+  await loginPage.fillUserNameInput(randomName);
+  await loginPage.fillPasswordInput(randomPassword);
   await loginPage.clickLoginButton();
   const errorTextMessage = await loginPage.getLoginErrorText();
   await expect(errorTextMessage).toBe(
