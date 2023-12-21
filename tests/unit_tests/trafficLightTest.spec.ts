a5686994b91fd7c557aa39bf5c811f32ae2ca1b2
Igor <merezhkoigor92@gmail.com>
2023-12-21 18:21:51 +0700

    deleted await

--- tests/unit_tests/trafficLightTest.spec.ts
+++ tests/unit_tests/trafficLightTest.spec.ts
@@ -36,51 +36,51 @@ test("Testing colorsAmount() function", async () => {
 
 for (const { number, light } of lights) {
   test(`Testing setLight() function with ${light} color`, async () => {
-    await trafficLight.setLight(`${number}`);
+    trafficLight.setLight(`${number}`);
     expect(trafficLight.light).toBe(`${light}`);
   });
 }
 
 for (const { number, light, nextLight } of lights) {
   test(`Testing next() function with light ${light}`, async () => {
-    await trafficLight.setLight(`${number}`);
-    await trafficLight.next();
+    trafficLight.setLight(`${number}`);
+    trafficLight.next();
     expect(nextLight).toBe(`${nextLight}`);
   });
 }
 
 test("Testing next() function with negative scenario", async () => {
-  await trafficLight.setLight(3);
-  await trafficLight.next();
+  trafficLight.setLight(3);
+  trafficLight.next();
   expect(trafficLight.light).toBeUndefined();
 });
 
 test("Testing isGreenActive() function", async () => {
-  await trafficLight.setLight(0);
+  trafficLight.setLight(0);
   expect(trafficLight.isGreenActive()).toBeTruthy();
 });
 
 test("Testing isGreenActive() function with negative scenario", async () => {
-  await trafficLight.setLight(faker.number.int({ min: 1, max: 2 }));
+  trafficLight.setLight(faker.number.int({ min: 1, max: 2 }));
   expect(trafficLight.isGreenActive()).toBeFalsy();
 });
 
 test("Testing isYellowActive() function", async () => {
-  await trafficLight.setLight(1);
+  trafficLight.setLight(1);
   expect(trafficLight.isYellowActive()).toBeTruthy();
 });
 
 test("Testing isYellowActive() function with negative scenario", async () => {
-  await trafficLight.setLight(0 | 2);
+  trafficLight.setLight(0 | 2);
   expect(trafficLight.isGreenActive()).toBeFalsy();
 });
 
 test("Testing isRedActive() function", async () => {
-  await trafficLight.setLight(2);
+  trafficLight.setLight(2);
   expect(trafficLight.isRedActive()).toBeTruthy();
 });
 
 test("Testing isRedActive() function with negative scenario", async () => {
-  await trafficLight.setLight(0 | 1);
+  trafficLight.setLight(0 | 1);
   expect(trafficLight.isGreenActive()).toBeFalsy();
 });
